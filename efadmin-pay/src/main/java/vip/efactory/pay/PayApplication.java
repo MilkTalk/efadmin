4d3a55bc5dcbeb30648fdb3cb87b5e6c7cbf33b5
dbdu <1797890817@qq.com>
2020-05-02 17:33:33 +0800

    添加支付模块

---
+++ efadmin-pay/src/main/java/vip/efactory/pay/PayApplication.java
@@ -0,0 +1,12 @@
+package vip.efactory.pay;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+
+}
