9eed27f09c67fc3e32d5e888495866d245da30ae
dbdu <1797890817@qq.com>
2020-05-02 17:52:10 +0800

    添加审批电子流模块

---
+++ efadmin-eflow/src/main/java/vip/efactory/eflow/EflowApplication.java
@@ -0,0 +1,13 @@
+package vip.efactory.eflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EflowApplication.class, args);
+    }
+
+}
